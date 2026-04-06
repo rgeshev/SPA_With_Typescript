084a47accd858e781492b3cd0ad7af28d32c433c
rgeshev <Rosen_Geshev19@abv.bg>
2026-04-06 21:57:48 +0300

    Update main.ts

--- Demo SPA with Typescript/src/main.ts
+++ Demo SPA with Typescript/src/main.ts
@@ -1,2 +1,35 @@
+import type { Routes } from './interfaces/routes.interface';
+import { Router } from './router';
 import './style.css'
+import { renderPostsView } from './views/posts.view';
+import { renderUsersView } from './views/users.view';
 
+const routes: Routes = {
+    '/posts': renderPostsView,
+    '/users': renderUsersView
+}
+const router = new Router(routes)
+
+function setupNavLinks() {
+    const postsLinkEl = document.getElementById('posts-link');
+    const usersLinkEl = document.getElementById('users-link');
+
+    if (postsLinkEl) {
+        postsLinkEl.addEventListener('click', e => {
+            e.preventDefault();
+
+            router.navigate('/posts');
+        })
+    };
+
+    if (usersLinkEl) {
+        usersLinkEl.addEventListener('click', e => {
+            e.preventDefault();
+
+            router.navigate('/users');
+        })
+    };
+}
+
+
+setupNavLinks();
