0c453187e5c5ebfa7ac8dad031f3c82d289ab7ca
rgeshev <Rosen_Geshev19@abv.bg>
2026-04-09 20:23:00 +0300

    updated files

--- Demo SPA with Typescript/src/main.ts
+++ Demo SPA with Typescript/src/main.ts
@@ -1,17 +1,20 @@
 import type { Routes } from './interfaces/routes.interface';
 import { Router } from './router';
 import './style.css'
+import { renderCreatePostView } from './views/createPost.view';
 import { renderPostsView } from './views/posts.view';
 import { renderUsersView } from './views/users.view';
 
 const routes: Routes = {
     '/posts': renderPostsView,
-    '/users': renderUsersView
+    '/users': renderUsersView,
+    '/posts/create': renderCreatePostView
 }
 const router = new Router(routes)
 
 function setupNavLinks() {
     const postsLinkEl = document.getElementById('posts-link');
+    const createPostLinkEl = document.getElementById('create-post-link')
     const usersLinkEl = document.getElementById('users-link');
 
     if (postsLinkEl) {
@@ -19,6 +22,14 @@ function setupNavLinks() {
             e.preventDefault();
 
             router.navigate('/posts');
+        })
+    };
+
+    if (createPostLinkEl) {
+        createPostLinkEl.addEventListener('click', e => {
+            e.preventDefault();
+
+            router.navigate('/posts/create');
         })
     };
 
--- Demo SPA with Typescript/src/views/createPost.view.ts
+++ Demo SPA with Typescript/src/views/createPost.view.ts
@@ -1,9 +1,32 @@
 import { PostsService } from "../services/posts.service";
+import { render } from "../utils/html";
 
-cosnt postsService = new PostsService();
+const postsService = new PostsService();
 
 export async function renderCreatePostView() {
     const template = `
-    
+    <form  id="create-post-form">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+
+        <label for="body">Title</label>
+        <input type="text" name="title" id="title" />
+
+        <button type="submit">Create post</button>
+    </form>
     `
+
+    render(template);
+
+    const formEl = document.getElementById('create-post-form');
+
+    if(formEl) {
+        formEl.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const formData = new FormData(e.target as HTMLFormElement);
+
+            const title = formData.get('title');
+        })
+    }
 }
